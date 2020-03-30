fca0e16384ebc5a99aa65b32184c15452f9abd84
BillChenIDY <31582542+BillChenIDY@users.noreply.github.com>
2020-03-30 19:29:12 +0800

    1.2.2版本更新
    精简了代码，modinfo页添加设置是否反鲜，默认不反鲜。

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "让姐妹骨灰罐保鲜！Keeping fresh!"
 description = "厌倦了在姐妹骨灰罐里的花枯萎？打开这个MOD将会让你不再烦恼这件事。\n Tired of withered flowers in sisturn? Opening this mod will keep you from worrying about it."
 author = "BCI_Chen"
-version = "1.1.1"
+version = "1.2.2"
 forumthread = ""
 dst_compatible = true
 don_starve_compatible = false
@@ -11,4 +11,20 @@ api_version = 10
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 server_filter_tags = {""}
-priority = -999
+priority = -999	--设置优先级
+
+configuration_options = 
+{
+    {
+        name = "Val",
+        label = "反鲜？refreshes？",
+        hover = "需要反鲜？Need to return fresh？",  
+        options = 
+        {
+		    {description = "需要/Yes", data = true},
+            {description = "不要/No", data = false},
+        },
+        default = false,
+    },
+
+}
--- modmain.lua
+++ modmain.lua
@@ -1,28 +1,28 @@
 GLOBAL.setmetatable(env,{__index=function(t,k) return GLOBAL.rawget(GLOBAL,k) end})
+local Vale = GetModConfigData("Val")
 
-local judge = TUNING.PERISH_FRIDGE_MULT	--获取冰箱回复数值
+local judge = TUNING.PERISH_FRIDGE_MULT	--获取冰箱的保鲜数值
 
-local function Reperishable(item)	--加新鲜度
-    item.components.perishable:ReducePercent(-.05)
-end
-
-local function DoReperishable(inst)	--厉遍有perishable组件的物品
+local function DoReperishable(inst)	
 	if  inst.components.container ~= nil then
-		for k,v in pairs(inst.components.container.slots) do
+		for k,v in pairs(inst.components.container.slots) do	--遍历有perishable组件的物品
 			if v.components.perishable then
-            Reperishable(v)
+				v.components.perishable:ReducePercent(-.05)	--加新鲜度
 			end
 		end
 	end
 end
 
 local function modsisturn_R(inst)
-	if  inst.components.container ~= nil then
-		if not inst:HasTag("fridge") 
+	if inst.components.container ~= nil then	--检测是否有这个组件
+		if not inst:HasTag("fridge")	--检测是否有这个标签，避免报错
 			and judge < 0 then	--如果开了回鲜，直接加冰箱标签完事
 			inst:AddTag("fridge")
-		else	--没有开就这样吧！
-			inst:DoPeriodicTask(1, DoReperishable, .05)
+		elseif Vale == true then	--如果需要单独反鲜的话
+			inst:DoPeriodicTask(1, DoReperishable, .05)	
+		else
+			inst:AddComponent("preserver")
+			inst.components.preserver:SetPerishRateMultiplier(0)
 		end
 	end
 end
